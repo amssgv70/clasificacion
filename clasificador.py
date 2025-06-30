492b3aa55d64fe1119812ceda3327c8509040f38
SUÁREZ SANFIEL ALFREDO MICHEL <35144063+amssgv70@users.noreply.github.com>
2025-06-30 15:30:07 -0300

    Update clasificador.py

--- clasificador.py
+++ clasificador.py
@@ -32,7 +32,7 @@ if not st.session_state.autenticado:
     st.stop()  # Detener todo lo demás hasta que esté autenticado
 
 # === CONFIGURACIÓN DE GEMINI ===
-API_KEY = os.getenv("GEMINI_API_KEY2")
+API_KEY = os.getenv("GEMINI_API_KEY_2")
 if not API_KEY:
     st.error("❌ API Key no configurada. Definila como variable de entorno GEMINI_API_KEY en Streamlit Cloud.")
     st.stop()
